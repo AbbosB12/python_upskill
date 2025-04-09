f52fe3547a4c8c2ab4171e3669af04b8a4ad9539
Abbos Shirinboev <58409711+AbbosB12@users.noreply.github.com>
2025-04-09 19:19:06 -0400

    Class python

--- day02/class_objects.py
+++ day02/class_objects.py
@@ -29,3 +29,28 @@ item2 = Item("Book",6.99)
 
 print(item1)
 print(item2)
+
+print(item1)
+print(item2)
+
+print(Item.made_in)
+print(Item.tariffs)
+
+Item.class_method()
+Item.static_method()
+
+"""
+Create Employee class:
+    instance variables: employee_name, job_title, salary
+    static_variables: pay_tax
+
+    instance methods: 
+        __init()__: declares and initalizes all the isnace variables
+        __str()__: creates string version of the object
+        work(): displaye  ${employee_name} is working
+
+    class method:
+        display_employee_tax_rate()
+
+
+"""
