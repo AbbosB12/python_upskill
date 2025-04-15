fbc6d5973510e7fac17e12ac6240d25b6b52981c
Abbos Shirinboev <58409711+AbbosB12@users.noreply.github.com>
2025-04-14 21:01:09 -0400

    abstractions in python

--- day04/abstractions2.py
+++ day04/abstractions2.py
@@ -1,6 +1,5 @@
+import math
 from abc import ABC, abstractmethod
-
-from day04.abstractions import circle
 
 
 class Shape(ABC):
@@ -17,24 +16,46 @@ class Square(Shape):
         self.side= side
 
     def area(self):
-        print("Calculating the square area")
+        return self.side ** 2
 
 
 class Circle(Shape):
-    pass
+
+    def __init__(self,radius):
+        self.radius= radius
+
+    def area(self):
+       return math.pi * (self.radius ** 2)
+
 class Cube(Shape):
-    pass
+
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return 6 * self.side ** 2
+
 class Cylinder(Shape):
-    pass
+    def __init__(self, radius, height):
+        self.radius = radius
+        self.height = height
+
+    def area(self):
+        return 2 * math.pi * self.radius * (self.radius + self.height)
 
 square = Square(5)
-# shape = Shape()
-# circle = Circle()
-# cube = Cube()
-# cylinder = Cylinder()
+circle = Circle(3)
+cube = Cube(4)
+cylinder = Cylinder(4,3)
 
 print(square)
-# print(circle)
-# print(cube)
-# print(cylinder)
-# print(shape)
+print(square.area())
+
+print(circle)
+print(circle.area())
+
+print(cube)
+print(cube.area())
+
+print(cylinder)
+print(cylinder.area())
